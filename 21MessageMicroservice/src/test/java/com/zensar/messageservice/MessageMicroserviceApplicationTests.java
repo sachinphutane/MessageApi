53a1809402c8a52d180c795cf71b6622ad905d96
sachinphutane <104205178+sachinphutane@users.noreply.github.com>
2022-06-15 12:27:22 +0530

    Add files via upload

---
+++ 21MessageMicroservice/src/test/java/com/zensar/messageservice/MessageMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zensar.messageservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MessageMicroserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
